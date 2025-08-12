842398d722394c4553e72cf59d6e0978bda4f877
ndurushima <ndu1994@gmail.com>
2025-08-12 11:45:17 -0500

    revering back to previous push

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,22 +6,20 @@ class GetRequester:
     def __init__(self, url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"):
         self.url = url
 
-    
-    def get_response_body(self, search_term: str) -> str:
+    def load_json(self):
         resp = requests.get(self.url)
         resp.raise_for_status()
-        return resp.content
-        
-        # people = self.load_json()
-        # term = search_term.strip().lower()
+        return json.loads(resp.text)
+    
+    def get_response_body(self, search_term: str) -> str:
+        people = self.load_json()
+        term = search_term.strip().lower()
 
-        # match = next((p for p in people if term in p.get('name', '').lower()), None)
+        match = next((p for p in people if term in p.get('name', '').lower()), None)
 
-        # name = match.get('name', 'Unknown')
-        # occupation = match.get('occupation', 'Unknown')
-        # return f"Employee Name: {name}\nOccupation: {occupation}"
-    def load_json(self):
-        return json.loads(self.get_response_body())
+        name = match.get('name', 'Unknown')
+        occupation = match.get('occupation', 'Unknown')
+        return f"Employee Name: {name}\nOccupation: {occupation}"
 
 if __name__ == "__main__":
     search_term = input("Enter an employee name: ")
